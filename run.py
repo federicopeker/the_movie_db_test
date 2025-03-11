21e2a30a21fe82ba869aafbd0b9eb1321965eab3
Federico Peker <federicopeker@gmail.com>
2025-03-11 19:53:04 -0300

    add openapi

--- run.py
+++ run.py
@@ -1,6 +1,25 @@
-from app import create_app
+import os
 
-app = create_app()
+from flask import Flask, send_from_directory
+from flask_swagger_ui import get_swaggerui_blueprint
+
+app = Flask(__name__)
+
+SWAGGER_URL = "/api/docs"
+API_URL = "/static/openapi.yaml"
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL, API_URL, config={"app_name": "Favorite Movies API"}
+)
+
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
+
+@app.route("/static/<path:path>")
+def send_static(path):
+    return send_from_directory("static", path)
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    debug_mode = os.getenv("TMDB_FLASK_DEBUG", "False").lower() in ["true", "1"]
+    app.run(debug=debug_mode)
